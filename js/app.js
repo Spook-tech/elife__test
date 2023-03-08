d83532c73172348187087081684b744e0227c0fe
Kostya Bondarenko <61065186+Spook-tech@users.noreply.github.com>
2023-03-08 08:35:23 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,131 +1,134 @@
-document.body.addEventListener('click', (e) => {
-  // Get the target element of the click event
-  const target = e.target;
+
+document.body.addEventListener('DOMContentLoaded', (event) => {
+  document.body.addEventListener('click', (e) => {
+    // Get the target element of the click event
+    const target = e.target;
+    
+    console.log(target);
+    if (target.classList.contains('like-button') || target.closest('.like-button')){
+      target.closest('.like-button').classList.toggle('add');
+    }
+    if (target.classList.contains('tour__tags__button')){
+      document.querySelector('.tour__tags').classList.toggle('active');
+    }
+  })
   
-  console.log(target);
-  if (target.classList.contains('like-button') || target.closest('.like-button')){
-    target.closest('.like-button').classList.toggle('add');
-  }
-  if (target.classList.contains('tour__tags__button')){
-    document.querySelector('.tour__tags').classList.toggle('active');
-  }
-})
-
-const reviewsSwiper = new Swiper('.reviews__swiper', {
-  slidesPerView: 3,
-  spaceBetween: 16,
-  pagination: {
-    el: '.reviews-pagination',
-    clickable: true,
-
-  },
-  breakpoints: {
-    0: {
-      slidesPerView: 1,
-      spaceBetween: 20
-    },
-    1194: {
-      slidesPerView: 3,
-      spaceBetween: 16
-    },
-  },
-  navigation: {
-    nextEl: '.reviews-button-next',
-    prevEl: '.reviews-button-prev',
-  },
-});
-
-const gallerySwiper = new Swiper('.gallery__swiper', {
-  spaceBetween: 20,
-  slidesPerView: 'auto',
-  pagination: {
-    el: '.gallery-pagination',
-    clickable: true,
-  },
-
-  breakpoints: {
-    991: {
-      spaceBetween: 20
-    },
-    1195: {
-      spaceBetween: 46
-    }
-  }
-});
-const newsSwiper = new Swiper('.articles-news__swiper', {
-  spaceBetween: 14,
-  slidesPerView: 1,
-  pagination: {
-    el: '.articles-news-pagination',
-    clickable: true,
-  },
-
-  breakpoints: {
-    1300: {
-      slidesPerView: 2,
-    }
-  }
-});
-
-const mySwiper = new Swiper('.tour-operators-swiper', {
-  spaceBetween: 50,
-  grid: {
-    rows: 3,
-    fill: "row",
-  },
-  pagination: {
-    el: '.tour-operators-pagination',
-    clickable: true,
-  },
+  const reviewsSwiper = new Swiper('.reviews__swiper', {
+    slidesPerView: 3,
+    spaceBetween: 16,
+    pagination: {
+      el: '.reviews-pagination',
+      clickable: true,
   
-  breakpoints: {
-    0: {
-      slidesPerView: 1,
-      spaceBetween: 15,
     },
-    834: {
-      slidesPerView: 1.5,
-      spaceBetween: 20,
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+        spaceBetween: 20
+      },
+      1194: {
+        slidesPerView: 3,
+        spaceBetween: 16
+      },
     },
-    1195: {
-      slidesPerView: 2.4,
+    navigation: {
+      nextEl: '.reviews-button-next',
+      prevEl: '.reviews-button-prev',
     },
-    1500: {
-      slidesPerView: 3,
+  });
+  
+  const gallerySwiper = new Swiper('.gallery__swiper', {
+    spaceBetween: 20,
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.gallery-pagination',
+      clickable: true,
+    },
+  
+    breakpoints: {
+      991: {
+        spaceBetween: 20
+      },
+      1195: {
+        spaceBetween: 46
+      }
     }
-  }
-});
-const mainPageSlider = new Swiper('.main-slider__swiper', {
-  spaceBetween: 25,
-  slidesPerView: 1,
-  pagination: {
-    el: '.main-slider__pagination',
-    clickable: true,
-  },
-  breakpoints: {
-    761: {
-      slidesPerView: 1.8,
+  });
+  const newsSwiper = new Swiper('.articles-news__swiper', {
+    spaceBetween: 14,
+    slidesPerView: 1,
+    pagination: {
+      el: '.articles-news-pagination',
+      clickable: true,
     },
-    991: {
-      slidesPerView: 2.5,
+  
+    breakpoints: {
+      1300: {
+        slidesPerView: 2,
+      }
+    }
+  });
+  
+  const mySwiper = new Swiper('.tour-operators-swiper', {
+    spaceBetween: 50,
+    grid: {
+      rows: 3,
+      fill: "row",
     },
-    1510: {
-      slidesPerView: 4,
+    pagination: {
+      el: '.tour-operators-pagination',
+      clickable: true,
     },
-  }
-});
-const toursSwiper = new Swiper('.tours-swiper', {
-  spaceBetween: 25,
-  slidesPerView: 1,
-  breakpoints: {
-    761: {
-      slidesPerView: 2,
+    
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+        spaceBetween: 15,
+      },
+      834: {
+        slidesPerView: 1.5,
+        spaceBetween: 20,
+      },
+      1195: {
+        slidesPerView: 2.4,
+      },
+      1500: {
+        slidesPerView: 3,
+      }
+    }
+  });
+  const mainPageSlider = new Swiper('.main-slider__swiper', {
+    spaceBetween: 25,
+    slidesPerView: 1,
+    pagination: {
+      el: '.main-slider__pagination',
+      clickable: true,
     },
-    1150: {
-      slidesPerView: 3,
-    },
-    1510: {
-      slidesPerView: 4,
-    },
-  }
+    breakpoints: {
+      761: {
+        slidesPerView: 1.8,
+      },
+      991: {
+        slidesPerView: 2.5,
+      },
+      1510: {
+        slidesPerView: 4,
+      },
+    }
+  });
+  const toursSwiper = new Swiper('.tours-swiper', {
+    spaceBetween: 25,
+    slidesPerView: 1,
+    breakpoints: {
+      761: {
+        slidesPerView: 2,
+      },
+      1150: {
+        slidesPerView: 3,
+      },
+      1510: {
+        slidesPerView: 4,
+      },
+    }
+  });
 });
